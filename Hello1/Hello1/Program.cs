73fad2a00a364506e4a197b670336e5a22d8e870
LiChangQi-513 <58315427+LiChangQi-513@users.noreply.github.com>
2019-11-29 03:14:32 +0800

    1

---
+++ Hello1/Hello1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
