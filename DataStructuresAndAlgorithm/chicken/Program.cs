4512b5e684368a0668425343c066204a2354594b
LiChangQi-513 <58315427+LiChangQi-513@users.noreply.github.com>
2019-12-20 00:38:44 +0800

    百钱买百鸡

---
+++ DataStructuresAndAlgorithm/chicken/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 0; x < 20; x++)
+            {
+                for (int y = 0; y < 33; y++)
+                {
+                    int z = 100 - x - y;
+                    if (x + y + z == 100 && 7 * x + 4 * y == 100)
+                    {
+                        Console.WriteLine("{0} {1} {2}", x, y, z);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
